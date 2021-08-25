32ed7964f1a4324029a32d3058847836d18f7d94
shaj <shajashwinar@gmail.com>
2021-08-25 10:00:11 +0530

    add Bank

---
+++ src/com/training/org/Bank.java
@@ -0,0 +1,11 @@
+package com.training.org;
+
+public class Bank {
+    public static void main(String[] args){
+        XYZBank x1 = new XYZBank();
+        System.out.println(x1);
+
+        XYZBank x2 =new XYZBank();
+        System.out.println(x2);
+    }
+}
